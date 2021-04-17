070eb4d37c67c96f2c466812b66c12cb5292c9cc
Sarah <sschlegel@myges.fr>
2021-04-17 17:08:39 +0200

    Cart items basic display

--- shopping-list/src/Cart.js
+++ shopping-list/src/Cart.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const cartItems = [
+    {name: "coca", price: 1.70, quantity: 2}
+] ;
+
+class CartProduct extends React.Component {
+    render() {
+        const item = this.props.item;
+        return (
+            <div className={item.name}>
+                <span>{item.name} : {item.quantity} * {item.price} € = {item.quantity * item.price} €</span>
+            </div>
+        )
+    }
+}
 
 class Cart extends React.Component {
     render() {
+        const rows = [] ;
+        cartItems.forEach((product) => {
+            rows.push(
+                <CartProduct
+                    item={product}
+                    key={product.name}
+                />
+            )
+        });
         return (
             <section className="Cart">
-            This is my cart
-        </section>
+                {rows}
+            </section>
         )
     }
 }
 
 ReactDOM.render(
     <Cart />,
-    document.getElementById('root')
+    document.getElementById('cart')
 )
 
 export default Cart
