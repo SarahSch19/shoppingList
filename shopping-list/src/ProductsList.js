41de72e5b3b1bc1ef66cf0a12ff0ff2436c285b5
Sarah <sschlegel@myges.fr>
2021-04-18 12:26:52 +0200

    refactor: Cart into ProductsList

--- shopping-list/src/ProductsList.js
+++ shopping-list/src/ProductsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './ProductsList.css';
+import './Cart.css';
 import bag from './bag.svg';
 
 const items = [
@@ -31,6 +32,10 @@ const items = [
     {name: "clémentine", price: 2.22},
     {name: "pitaya", price: 16.5},
     {name: "mangue", price: 3.99}
+] ;
+
+const cartItems = [
+    {name: "coca", price: 1.70, quantity: 2}
 ] ;
 
 function addToCart (itemName) {
@@ -98,9 +103,40 @@ class FilterProductsList extends React.Component {
     }
 }
 
+class CartProduct extends React.Component {
+    render() {
+        const item = this.props.item;
+        return (
+            <div className={item.name}>
+                <span>{item.name} : {item.quantity} * {item.price} € = {item.quantity * item.price} €</span>
+            </div>
+        )
+    }
+}
+
+class Cart extends React.Component {
+    render() {
+        const rows = [] ;
+        cartItems.forEach((product) => {
+            rows.push(
+                <CartProduct
+                    item={product}
+                    key={product.name}
+                />
+            )
+        });
+        return (
+            <section className="Cart">
+                {rows}
+            </section>
+        )
+    }
+}
+
 ReactDOM.render(
     <main>
         <FilterProductsList />
+        <Cart />
     </main>,
     document.getElementById('root')
 );
